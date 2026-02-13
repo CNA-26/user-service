e17304ba6718555cd6c161704f64f06f2148b369
arcaadser <149475007+arcaadser@users.noreply.github.com>
2026-02-13 17:14:52 +0200

    docs: Add JWT documentation

--- src/swagger.js
+++ src/swagger.js
@@ -5,7 +5,7 @@ const options = {
         openapi: '3.0.0', info: {
             title: 'User service API',
             version: '1.0.0',
-            description: 'Welcome to User Service API. Feel free to try requests yourself.\n' + '- [Our repository](https://github.com/CNA-26/user-service/)\n' + '- If you need admin rights [create an issue](https://github.com/CNA-26/user-service/issues/new/choose) writing @mentions of our [team members](https://github.com/orgs/CNA-26/teams/user-service) in it or otherwise contact us.',
+            description: 'Welcome to User Service API. Authorization is done using JSON Web Token which is documented in our README and emitted in response by /api/auth/login endpoint. Feel free to try requests yourself.\n' + '- [Our repository](https://github.com/CNA-26/user-service/)\n' + '- If you need admin rights [create an issue](https://github.com/CNA-26/user-service/issues/new/choose) writing @mentions of our [team members](https://github.com/orgs/CNA-26/teams/user-service) in it or contact us by any other means.',
         },
 
         components: {
